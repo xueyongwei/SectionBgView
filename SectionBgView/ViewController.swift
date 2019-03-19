ae629571d17271b3ff3f7d2244ce0452825ddaa4
xueyongwei <xueyongwei@ylmf.com>
2019-03-19 17:23:28 +0800

    Initial Commit

---
+++ SectionBgView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SectionBgView
+//
+//  Created by 薛永伟 on 2019/3/19.
+//  Copyright © 2019年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
